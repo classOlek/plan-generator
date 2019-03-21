bf4acea175e7c9229e61d3fc6ff83681c061bf3b
classOlek <aleksander.bielas@gmail.com>
2019-03-21 01:41:49 +0100

    Add project files.

---
+++ TimetableGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimetableGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
