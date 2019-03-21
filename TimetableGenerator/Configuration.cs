3947040b408392938fd026dbca861ac5ef91375a
classOlek <aleksander.bielas@gmail.com>
2019-03-21 22:30:36 +0100

    MongoDB Database Test

---
+++ TimetableGenerator/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimetableGenerator
+{
+    public class Configuration
+    {
+        public static string DatabaseIpAddress = "localhost";
+        public static int DatabasePort = 27017;
+        public static string DatabaseName = "TimetableGenerator";
+    }
+}
