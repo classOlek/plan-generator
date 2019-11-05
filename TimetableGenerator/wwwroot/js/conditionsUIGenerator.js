06f3113fe559a6461a9fd89c5a7ebb93383b19e5
classOlek <aleksander.bielas@gmail.com>
2019-11-05 01:10:45 +0100

    - Fix timetable display - Responsive main menu layout

--- TimetableGenerator/wwwroot/js/conditionsUIGenerator.js
+++ TimetableGenerator/wwwroot/js/conditionsUIGenerator.js
@@ -3,27 +3,25 @@
     element.id = `listItem${elementId}`;
     element.classList.add('list-group-item');
     element.classList.add('noClassesBefore');
-    element.innerHTML = `<div class="row">
-                        <div class="col-md-7">
-                            <div class="form-group row">
-                                <label for="inputEmail3" class="col-sm-6 col-form-label">No classes before</label>
-                                <div class="col-sm-4">
-                                    <select class="form-control" id="timeSelect`+ elementId + `">
-                                        <option>8:00</option>
-                                        <option>9:00</option>
-                                        <option>10:00</option>
-                                        <option>11:00</option>
-                                        <option>12:00</option>
-                                        <option>13:00</option>
-                                        <option>14:00</option>
-                                        <option>15:00</option>
-                                        <option>16:00</option>
-                                        <option>17:00</option>
-                                    </select>
-                                </div>
-                            </div>
+    element.innerHTML = `<div class="container"><div class="row">
+                        <div class="col" style="font-size: 12px;">
+                            No classes before
                         </div>
                         <div class="col-md-3">
+                            <select class="form-control" id="timeSelect`+ elementId + `">
+                                <option>8:00</option>
+                                <option>9:00</option>
+                                <option>10:00</option>
+                                <option>11:00</option>
+                                <option>12:00</option>
+                                <option>13:00</option>
+                                <option>14:00</option>
+                                <option>15:00</option>
+                                <option>16:00</option>
+                                <option>17:00</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
                             <select class="form-control" id="daySelect`+ elementId + `">
                                 <option>Everyday</option>
                                 <option>Monday</option>
@@ -33,10 +31,10 @@
                                 <option>Friday</option>
                             </select>
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-1">
                             <button id="removeElement" class="btn btn-danger" onclick="removeElement('listItem`+ elementId + `');">X</button>
                         </div>
-                    </div>`;
+                    </div></div>`;
     document.getElementById(parentId).appendChild(element);
     document.getElementById(`timeSelect${elementId}`).value = hour;
     document.getElementById(`daySelect${elementId}`).value = day;
@@ -47,27 +45,25 @@ function addNoClassesAfter(elementId, parentId, day, hour) {
     element.id = `listItem${elementId}`;
     element.classList.add('list-group-item');
     element.classList.add('noClassesAfter');
-    element.innerHTML = `<div class="row">
-                        <div class="col-md-7">
-                            <div class="form-group row">
-                                <label for="inputEmail3" class="col-sm-6 col-form-label">No classes after</label>
-                                <div class="col-sm-4">
-                                    <select class="form-control" id="timeSelect`+ elementId + `">
-                                        <option>8:00</option>
-                                        <option>9:00</option>
-                                        <option>10:00</option>
-                                        <option>11:00</option>
-                                        <option>12:00</option>
-                                        <option>13:00</option>
-                                        <option>14:00</option>
-                                        <option>15:00</option>
-                                        <option>16:00</option>
-                                        <option>17:00</option>
-                                    </select>
-                                </div>
-                            </div>
+    element.innerHTML = `<div class="container"><div class="row">
+                        <div class="col" style="font-size: 12px;">
+                            No classes after
                         </div>
                         <div class="col-md-3">
+                            <select class="form-control" id="timeSelect`+ elementId + `">
+                                <option>8:00</option>
+                                <option>9:00</option>
+                                <option>10:00</option>
+                                <option>11:00</option>
+                                <option>12:00</option>
+                                <option>13:00</option>
+                                <option>14:00</option>
+                                <option>15:00</option>
+                                <option>16:00</option>
+                                <option>17:00</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
                             <select class="form-control" id="daySelect`+ elementId + `">
                                 <option>Everyday</option>
                                 <option>Monday</option>
@@ -77,10 +73,10 @@ function addNoClassesAfter(elementId, parentId, day, hour) {
                                 <option>Friday</option>
                             </select>
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-1">
                             <button id="removeElement" class="btn btn-danger" onclick="removeElement('listItem`+ elementId + `');">X</button>
                         </div>
-                    </div>`;
+                    </div></div>`;
     document.getElementById(parentId).appendChild(element);
     document.getElementById(`timeSelect${elementId}`).value = hour;
     document.getElementById(`daySelect${elementId}`).value = day;
